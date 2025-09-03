a1a754e5c330e412f4f351b12e1ddb76ff0cb0ea
Omar Estrada <omarestrada908@gmail.com>
2025-09-03 01:53:10 -0500

    Made Function within UserInputNormalizer.java as final.

--- src/main/java/com/omar/user_management_api/util/UserInputNormalizer.java
+++ src/main/java/com/omar/user_management_api/util/UserInputNormalizer.java
@@ -3,12 +3,12 @@ package com.omar.user_management_api.util;
 import java.util.function.Function;
 
 public class UserInputNormalizer {
-    public static Function<String, String> catchNullValuesForString = defaultIfNull("");
-    public static Function<String, String> trimString = String::trim;
-    public static Function<String, String> lowercaseString = String::toLowerCase;
+    public static final Function<String, String> catchNullValuesForString = defaultIfNull("");
+    public static final Function<String, String> trimString = String::trim;
+    public static final Function<String, String> lowercaseString = String::toLowerCase;
 
-    public static Function<String, String> normalizeEmail =  catchNullValuesForString.andThen(trimString).andThen(lowercaseString);
-    public static Function<String, String> normalizeName = catchNullValuesForString.andThen(trimString);
+    public static final Function<String, String> normalizeEmail =  catchNullValuesForString.andThen(trimString).andThen(lowercaseString);
+    public static final Function<String, String> normalizeName = catchNullValuesForString.andThen(trimString);
 
     public static <T> Function <T, T> defaultIfNull(T fallback) {
         return input -> input == null ? fallback : input;
